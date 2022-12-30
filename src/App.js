e78a821925a8d19b468254eba5fba47d6901555b
dragon <1573511441@qq.com>
2022-12-30 10:17:18 +0800

    feat: 确定组件库样式方案

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>asdf</h1>
+        <h2>asdf</h2>
+        <h3>asdf</h3>
       </header>
     </div>
   );
